fc609b4bafffd26d8b50162fd74c4336d3896951
GEUNA <geuna0204@gmail.com>
2021-04-05 16:58:47 +0900

    [Fix] Input component 수정

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,21 +1,21 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { Field } from 'formik';
-import styled, { css } from 'styled-components';
-import PropTypes from 'prop-types';
+import styled, { css } from 'styled-components'
+import propTypes from 'prop-types'
 
-// condition styles
-const isFocused = css`
-  ${props =>
-    props.isFocused &&
-    css`
-      & + label {
-        top: -30px;
-        left: 0px;
-      }
-    `}
-`;
+const StyledInputLabel = styled.label`
+  font-size: 1.5em;
+  transition: all 0.3s;
+  position: absolute;
+  top: 5px;
+  left: 10px;
+`
 
-//styled components
+const StyledInput = styled.input`
+  width: 100%;
+  padding: 10px;
+  font-size: 1em;
+  box-sizing: border-box;
+`
+
 const StyledInputContainer = styled.div`
   position: relative;
   margin-bottom: 40px;
@@ -26,97 +26,34 @@ const StyledInputContainer = styled.div`
   label {
     font-size: 1.5em;
   }
-`;
+`
 
-const StyledInputLabel = styled.label`
-  font-size: 1.5em;
-  transition: all 0.3s;
-  position: absolute;
-  top: 5px;
-  left: 10px;
-`;
-const StyledInput = styled.input`
-  width: 100%;
-  padding: 10px;
-  font-size: 1em;
-  box-sizing: border-box;
-  ${isFocused}
-`;
-
-const StyledTextArea = styled.textarea`
-  width: 100%;
-  display: block;
-  margin-top: 20px;
-  padding: 10px;
-  font-size: 1.3em;
-  box-sizing: border-box;
-`;
-
-const Input = ({ field, form: { touched, errors }, type }) => {
-  const [isFocused, setIsFocused] = useState(false);
-  const inputRef = useRef();
-
-  useEffect(() => {
-    if (type !== 'checkbox' && type !== 'textarea') {
-      inputRef.current.addEventListener('focus', () => setIsFocused(true));
-    }
-  }, [type]);
-
-  const renderInput = () => {
-    switch (type) {
-      case 'textarea':
-        return (
-          <StyledInputContainer>
-            <label htmlFor={field.name}>
-              {field.name.charAt(0).toUpperCase() + field.name.slice(1)}
-            </label>
-            <StyledTextArea id={field.name} rows='6' {...field} />
-          </StyledInputContainer>
-        );
-      case 'checkbox':
-        return (
-          <>
-            <label>
-              <Field type='checkbox' name='services' value={field.name} />
-              {field.name}
-            </label>
-          </>
-        );
-      default:
-        return (
-          <StyledInputContainer>
-            <StyledInput
-              type={type}
-              ref={inputRef}
-              id={field.name}
-              isFocused={isFocused}
-              {...field}
-            />
-            <StyledInputLabel htmlFor={field.name}>
-              {field.name.charAt(0).toUpperCase() + field.name.slice(1)}
-            </StyledInputLabel>
-            {errors[field.name] && touched[field.name] ? (
-              <span style={{ color: 'red' }}>{errors[field.name]}</span>
-            ) : (
-              <span style={{ opacity: 0 }}>Error</span>
-            )}
-          </StyledInputContainer>
-        );
-    }
-  };
-  return renderInput();
-};
+export default function Input({ type, title, name }) {
+  return (
+    <>
+    <StyledInputContainer>
+      <StyledInput 
+        type={type}
+        id={name}
+        name={name}
+      />
+      <StyledInputLabel
+        htmlFor={name}
+      >
+        {title}
+      </StyledInputLabel>
+    </StyledInputContainer>
+    </>
+  )
+}
 
 Input.propTypes = {
-  type: PropTypes.oneOf(['text', 'email', 'checkbox', 'textarea']),
-  field: PropTypes.object,
-  form: PropTypes.object
-};
+  type: propTypes.oneOf(['text', 'email']),
+  name: propTypes.string.isRequired,
+}
 
 Input.defaultProps = {
   type: 'text'
-};
+}
 
-Input.displayName = 'Input';
-
-export default Input;
+Input.displayName = 'Input'
